188362c6b7ff1cf35450f5d34556a7eb3f618443
AlexiSkyline <ilegal_sprite@outlook.com>
2021-09-29 13:36:07 -0500

    finished adding the functions to our custom hook

--- src/Components/Formulario.js
+++ src/Components/Formulario.js
@@ -4,7 +4,17 @@ import styles from './Formulario.module.css';
 
 export const Formulario = () => {
 
-    const [ categoria, SelectNoticias ] = useSelect();
+    const OPCIONES = [
+        { value: 'general', label: 'General' },
+        { value: 'business', label: 'Negocios' },
+        { value: 'entertainment', label: 'Entretenimiento' },
+        { value: 'health', label: 'Salud' },
+        { value: 'science', label: 'Ciencia' },
+        { value: 'sport', label: 'Deportes' },
+        { value: 'technology', label: 'Tecnología' },
+    ];
+
+    const [ categoria, SelectNoticias ] = useSelect( 'general', OPCIONES );
     return (
         <div className={`${ styles.buscador } row`}>
             <div className="col s12 m8 offset-m2">
@@ -12,7 +22,6 @@ export const Formulario = () => {
                     <h2 className={ styles.heading }>Encuentra Noticias por Categoría</h2>
 
                     <SelectNoticias />
-                    
                     <div className="input-field col s12">
                         <input 
                             type="submit"
--- src/hooks/useSelect.js
+++ src/hooks/useSelect.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react'
 
 export const useSelect = (stateInicial, opciones) => {
     
-    const [state, setState] = useState('');
+    const [state, setState] = useState(stateInicial);
     
-    const SelectNoticias = () => {
+    const SelectNoticias = () => ( 
         <select
             className="browser-default"
+            onChange={ e => setState( e.target.value ) }
         >
             <option value="">-- Seleccione --</option>
+            { opciones.map( opcion => (
+                    <option key={ opcion.value }  value={ opcion.value }>{ opcion.label }</option>
+                ))
+            }
         </select>
-    };
+    );
 
     return [ state, SelectNoticias ];
 }
