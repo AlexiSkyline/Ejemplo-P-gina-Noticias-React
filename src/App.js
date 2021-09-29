2c9761017855add9ef049dfbde06acf479cf7f77
AlexiSkyline <ilegal_sprite@outlook.com>
2021-09-28 22:53:27 -0500

    we add the Formulario Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Formulario } from './Components/Formulario';
 import { Header } from './Components/Header';
 
 function App() {
   return (
     <>
         <Header titulo="buscador de noticias"/>
+
+        <div className="container white">
+          <Formulario />
+        </div>
     </>
   );
 }
